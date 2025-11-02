b0123a1207c595945ab1bd54085610588ee51f7d
Alok Paranjape <alok.paranjape2@gmail.com>
2025-11-01 22:30:15 -0400

    added basic html page

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//adding this to get files from the subfolder
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js inside Docker!");
+  res.send("Hello from Node.js inside Docker! (modified this message)");
 });
 
 app.listen(port, () => {
