7b68010c3f38b18928e9a860201caf86a6a09c3b
Alok Paranjape <alok.paranjape2@gmail.com>
2025-11-16 14:13:13 -0500

    app.py

--- init.sql
+++ init.sql
@@ -18,3 +18,33 @@ INSERT INTO users (username, password, role) VALUES ('admin', 'scrypt:32768:8:1$
 INSERT INTO inventory (item_name, item_quantity) VALUES
 ('Apples', 10),
 ('Oranges', 15);
+
+-- Adding a function that handles notifications
+CREATE OR REPLACE FUNCTION notify_database_change()
+RETURNS TRIGGER AS $$
+DECLARE 
+  payload JSON;
+BEGIN
+  payload = json_build_object(
+    -- should just be inventory
+    'table', TG_TABLE_NAME,
+    'action', TG_OP,
+    -- had to add this, since it'll change when there's a new item or one is deleted
+    -- TODO?: also need to add function for changing IDs
+    'id', COALESCE(OLD.id, NEW.id)
+  );
+
+  PERFORM pg_notify('inventory_events', payload::text);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- Create trigger on INSERT/UPDATE/DELETE/CREATE
+-- whenever those happen, try and run the function
+CREATE TRIGGER inventory_trigger
+AFTER CREATE OR INSERT OR UPDATE OR DELETE ON inventory
+FOR EACH ROW EXECUTE FUNCTION notify_database_change();
+
+
